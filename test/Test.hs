a38a967bb6a465147bc9a5e266d568e989be9bd5
Gizmo Mk. 0 <gizmo.mk0@gmail.com>
2018-12-11 16:10:26 +0100

    Experiment with `fix`

--- test/Test.hs
+++ test/Test.hs
@@ -3,53 +3,54 @@
 module Test where
 --
 import Data.List (intercalate)
+import Control.Monad.Fix (fix)
 
 type Vector2 = (Int, Int)
 --
 
 type Index = Int
 
-data Graph = Graph
+data Graph = MkGraph
            { addNode :: Vector2 -> Graph
            , cursors :: [Node] }
 
-data Node = Node
+data Node = MkNode
           { ix       :: Index
           , position :: Vector2
           , pins     :: [Pin] }
-data Pin = Pin
+
+data Pin = MkPin
          { ix        :: Index
          , pinType   :: PinType
          , parent    :: Node
          , otherPins :: [Pin] }
+         
 data PinType = InputPin | OutputPin deriving (Eq)
 
 mkGraph :: Graph
-mkGraph =
-    let newGraph = Graph (addNode' newGraph 0) []
-        addNode' g ix p =
-            let g' = Graph (addNode' g' (ix + 1)) (mkNode ix p : cursors g)
-            in  g'
-    in  newGraph
+mkGraph = fix $ \graph -> MkGraph (addNode' graph 0) []
+    where
+    addNode' g ix p = fix $ \g' -> MkGraph (addNode' g' (ix + 1)) (mkNode ix p : cursors g)
 
-mkNode :: Int -> Vector2 -> Node
-mkNode ix p = Node ix p []
+
+mkNode :: Index -> Vector2 -> Node
+mkNode ix p = MkNode ix p []
 
 instance Show Graph where
-    show (Graph _ cursors') = "Graph count: " ++ show (length cursors') ++ "\n" ++ intercalate "\n" (fmap show cursors')
+    show (MkGraph _ cursors') = "Graph count: " ++ show (length cursors') ++ "\n" ++ intercalate "\n" (fmap show cursors')
 
 instance Show Node where
-    show (Node ix' position' pins') = "Node " ++ show ix' ++ " - pin count: " ++ show (length pins')
+    show (MkNode ix' position' pins') = "Node " ++ show ix' ++ " - pin count: " ++ show (length pins')
 
 --
 
-data TestNode = TestNode
+data TestNode = MkTestNode
               { val :: Int
               , prev :: TestNode
               , next :: TestNode }
 
 instance Show TestNode where
-    show (TestNode val' prev' next') =
+    show (MkTestNode val' prev' next') =
         show (val (prev prev')) ++ " " ++
         show (val prev') ++
         " [" ++ show val' ++ "] " ++
@@ -57,24 +58,24 @@ instance Show TestNode where
         show (val (next next'))
 
 mkTest :: TestNode
-mkTest = let testNode = TestNode 1 prevNode nextNode
-             prevNode = TestNode 0 prevNode testNode
-             nextNode = TestNode 2 testNode nextNode
+mkTest = let testNode = MkTestNode 1 prevNode nextNode
+             prevNode = MkTestNode 0 prevNode testNode
+             nextNode = MkTestNode 2 testNode nextNode
          in  testNode
 
 stepPrev :: TestNode -> TestNode
-stepPrev (TestNode _ p _) = p
+stepPrev (MkTestNode _ p _) = p
 
 stepNext :: TestNode -> TestNode
-stepNext (TestNode _ _ n) = n
+stepNext (MkTestNode _ _ n) = n
 
 changeNext :: TestNode -> TestNode -> TestNode
-changeNext newNext (TestNode v p _) = TestNode v p newNext
+changeNext newNext (MkTestNode v p _) = MkTestNode v p newNext
 
 changePrev :: TestNode -> TestNode -> TestNode
-changePrev newPrev (TestNode v _ n) = TestNode v newPrev n
+changePrev newPrev (MkTestNode v _ n) = MkTestNode v newPrev n
 
 changeVal :: Int -> TestNode -> TestNode
-changeVal v (TestNode _ p n) =
-    let newNode = TestNode v (changeNext newNode p) (changePrev newNode n)
+changeVal v (MkTestNode _ p n) =
+    let newNode = MkTestNode v (changeNext newNode p) (changePrev newNode n)
     in  newNode
